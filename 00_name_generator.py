411a8937e76d56d99487e6cbdc57eeae84670c3d
Dominik Szyja <dery1996@gmail.com>
2016-10-11 20:58:13 +0200

    Fixed

--- 00_name_generator.py
+++ 00_name_generator.py
@@ -10,11 +10,11 @@ def Generate_Word(word_len=15, word=''):
     '''This function generate Words (Random Letters) with syllabes that it
        could make sense'''
 
-    word_length = RAND(5, word_len)
-    syllabe_rand = syllabes[RAND(0, len(syllabes) - 1)]
+    word_length = RANDOM(5, word_len)
+    syllabe_rand = syllabes[RANDOM(0, len(syllabes) - 1)]
 
     for i in range(0, word_length):
-        letter_random = letters[RAND(0, len_letters)]
+        letter_random = letters[RANDOM(0, len_letters)]
         if i == 0:
             word += letter_random.upper()
         if i + len(syllabe_rand) >= word_length:
